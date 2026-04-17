043d3f632ec37c96290f6bdfb8d5ece182375109
Nicola De Nisco <nicoladenisco@gmail.com>
2026-04-17 07:40:46 +0200

    java

---
+++ sc-java/src/main/java/org/opensc/ScJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.opensc;
+
+/**
+ *
+ * @author Nicola De Nisco
+ */
+public class ScJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
